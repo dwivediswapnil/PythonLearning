5701f8d371ece81a1995b4192d6abfa02f07de0b
Swapnil Dwivedi <56720953+dwivediswapnil@users.noreply.github.com>
2022-12-28 12:43:46 +0000

    comments ,etc

---
+++ Basics/comments.py
@@ -0,0 +1,21 @@
+print( 'Hi my name is "Swapnil" \n and i am a good coder')
+'''
+This is also known as multiline comments or docstring.
+'''
+print("Hello World")
+print(5)
+print("Bye")
+print(12*12)
+#HI, plz don't remove this code 
+print("Hi i am a good boy \n and i am a perfect software engineer")
+print("Hey",8,"Rey",9,sep="~",end="001\n")
+#sep="~",end="001\n" are optional
+print("Swapnil")
+a=5
+print(type(a))
+print(a)
+#everything in python is an object
+
+
+
+
