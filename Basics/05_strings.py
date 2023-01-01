f9c2e530e6a8ba26f6b294dede9d811791f1bcd0
Swapnil Dwivedi <56720953+dwivediswapnil@users.noreply.github.com>
2023-01-01 14:32:48 +0000

    extracting character from strings

---
+++ Basics/05_strings.py
@@ -0,0 +1,15 @@
+# ""/'' means the same for strings in python.
+
+#Multiline string .
+st ='''
+This is python where i am coding good. Also 
+i am doing great in python
+'''
+print(st)
+
+name= "Swapnil"
+print(name[0])#indexing starts from 0.
+print(name[1])
+# In python , string is like an array of characters.
+for i in name:
+    print(i) #printing character by character
