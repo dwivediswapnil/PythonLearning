73c4b1aa4d80d46a20481e43d33ed8809e76c0b5
Swapnil Dwivedi <56720953+dwivediswapnil@users.noreply.github.com>
2023-01-01 13:20:49 +0000

    Calculator pg.

---
+++ Basics/02_calculator.py
@@ -0,0 +1,34 @@
+print(5+6)
+
+#this will result in 2.5 where .5 will be removed using "Floor division" operator
+print(15//6)
+
+# "%" gives remainder.
+print(15%6)
+
+# exponential operator "**"
+print (2**3)
+
+
+
+#Program for calculator
+a=10
+b=8
+inputFromUser= input("What do you want to do ?")
+if inputFromUser =="Add":
+    print(a+b)
+elif inputFromUser == "multiply":
+    print(a*b)   
+elif inputFromUser == "subtract":
+    print(a-b)
+elif inputFromUser == "division":
+    print(a/b)
+elif inputFromUser == "floor division":
+    print(a//b)                   
+elif inputFromUser == "mod":
+    print(a%b)
+elif inputFromUser == "exponent":
+    print(a**b)            
+
+
+
