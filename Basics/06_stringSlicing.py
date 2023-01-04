2f76e7ad8673b6e90195c8e2ebb742494e25c551
Swapnil Dwivedi <56720953+dwivediswapnil@users.noreply.github.com>
2023-01-04 16:33:14 +0000

    stringSlicing

---
+++ Basics/06_stringSlicing.py
@@ -0,0 +1,11 @@
+fruit = "Mango"
+mangoLen = len(fruit)
+print(mangoLen)
+print(fruit[0:4])#including 0 but not 4
+print(fruit[1:4])
+print(fruit[:5])
+print(fruit[0:-1])#Negative means Python will put as [len(fruit)-1 in the present case]
+
+nm="Harry"
+print(len(nm))
+print(nm[-4:-2])
