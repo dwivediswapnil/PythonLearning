ad97b57f0520227a354295121e137ef0586ceb95
Swapnil Dwivedi <56720953+dwivediswapnil@users.noreply.github.com>
2023-01-05 17:35:31 +0000

    for loop

---
+++ Basics/12_forLoop.py
@@ -0,0 +1,15 @@
+name = "Swapnil"
+for i in name:
+    print (i,end=",")
+
+colors = ["Red","Green","Yellow","White"]
+for color in colors:
+    print ("\n",color)
+    for i in color:
+        print (i)
+
+for k in range(1,20):# this will go from 0 to 4(-1 from the last given value, i.e n-1)
+    print (k)
+
+for k in range(1,20,3):
+    print (k)    
