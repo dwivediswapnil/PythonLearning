960a15a42830f89b5dd051f2cd4b076e103a5dd0
Swapnil Dwivedi <56720953+dwivediswapnil@users.noreply.github.com>
2023-01-15 18:46:24 +0000

    methods in tuple

--- Basics/17_tuple.py
+++ Basics/17_tuple.py
@@ -27,3 +27,33 @@ print(tup1)
 
 even_num=(2,4,8,6,10,12,14,16,18,20,22,24)
 print(even_num[0:8:3])
+
+#methods in tuple 
+#to change the content of the tuple , we 
+#should firstly change it into list , then 
+#change into tuple.
+countries=("India","Israel","Australia","Africa","America")
+print(countries)
+temp=list(countries)
+print(temp)
+temp.append("Russia")
+temp.pop(3)
+temp[2]="Finland"
+countries = tuple(temp)
+print(countries)
+
+countries2=("Algeria","Moscow","Iran")
+#on concatenation, a new tuple wil be formed
+print(countries+countries2)
+#count
+print(countries2.count("Algeria"))
+# to get the first occurence 
+res= countries.index("Israel")
+print(res)
+tuple1=(0,2,3,4,5,6,7,89,5)
+#(3,0,3) means that search for 3 between 0
+#to <3
+res1= tuple1.index(3,0,3)
+print(res1)
+
+print(len(tuple1))
