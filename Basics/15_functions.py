3ce1093e6a4798d4904f33b3d2d4aacbe1c28c6d
Swapnil Dwivedi <56720953+dwivediswapnil@users.noreply.github.com>
2023-01-15 17:32:00 +0000

    created some functions for different types of arguments .

--- Basics/15_functions.py
+++ Basics/15_functions.py
@@ -8,3 +8,26 @@ def greater(a,b):
 for i in range(12):
     if(i==3):
         greater(i,5)
+
+#also an example of keyword arguments.
+def sum(a, b=4):
+    print(a+b)
+sum(b=4,a=2)
+
+#Arbitrary arguments
+def average(*numbers):
+    sum=0
+    for i in numbers:
+        sum=sum+i
+    print("Average is : ",sum/len(numbers))
+#it will take input as a tuple.
+
+average(2,3,4,5,6,7,8)    
+
+#Keyword arbitrary arguments 
+def name(**name):
+    print(type(name))
+    print("Hello,",name["fname"],name["mname"],name["lname"])
+
+name(mname="Ashok", lname = "Dwivedi", fname="Swapnil")
+
