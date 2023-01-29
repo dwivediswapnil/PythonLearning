16ca8f0cb2f8ebc873c329929a3afbc4d5d654c2
Swapnil Dwivedi <56720953+dwivediswapnil@users.noreply.github.com>
2023-01-29 19:01:21 +0000

    docstring

---
+++ Basics/19_docString.py
@@ -0,0 +1,5 @@
+# should be placed right below the function definition
+def even():
+    """ This function returns an even number"""
+    print("even")
+print(even.__doc__)#will return the docstring
