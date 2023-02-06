3f2c8784a0cb1b262c3537f4a3b48104f6f80d98
Swapnil Dwivedi <56720953+dwivediswapnil@users.noreply.github.com>
2023-02-06 13:12:07 +0000

    set

---
+++ Basics/20_set.py
@@ -0,0 +1,15 @@
+s={1,2,3,4,5,3}
+print(s)
+#set do not allow suplicate elements.
+#Set is not ordered
+
+str={}
+print(type(str))
+#this is becoz set and dictionary both are denoted using {}
+#so for defining empty set 
+str= set()
+print(type(str))
+
+str2={"Carla",23,"Bfr",1,23,2.23}
+for i in str2:
+    print(i)
