b0ddcb3c9b34ff0beecdb4a49cdacb5e08ea0e64
Swapnil Dwivedi <56720953+dwivediswapnil@users.noreply.github.com>
2023-08-07 19:57:25 +0000

    @dataclass

--- Basics/25_decorator(@dataclass).py
+++ Basics/25_decorator(@dataclass).py
@@ -5,5 +5,11 @@ class Person:
     last_name:str
 #@dataclass automatically create __init__() behind the scene and initialize    
 
+    def age_of_person(self,age,standard):
+        print(f'{standard} is {age} years of age.')
+
 person = Person("Swapnil","Dwivedi")
 print(person.first_name , person.last_name)    
+
+person.age_of_person(23,"3rd")
+
